a6cadbd4d42cb811dd5b0bf4229dc068b663d765
Syota Tsuda <s-tsuda@magicant.co.jp>
2022-11-14 03:35:34 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  dict = {dev: 'develop'}
+  
+  dict.each{|key, val|
+    namespace key do
+      scope :nft do
+        post 'balance', to: val+'#balance'
+        post 'list_collections', to: val+'#list_collections'
+        post 'collection_detail', to: val+'#collection_detail'
+        post 'clone', to: val+'#clone'
+        post 'mint', to: val+'#mint'
+        post 'burn', to: val+'#burn'
+        post 'burn_to_mint', to: val+'#burn_to_mint'
+        post 'transfer', to: val+'#transfer'
+        post 'list_tokens', to: val+'#list_tokens'
+        post 'list_tokens_by_collection', to: val+'#list_tokens_by_collection'
+        post 'token_detail', to: val+'#token_detail'
+        post 'list_user_tokens', to: val+'#list_user_tokens'
+        post 'list_user_tokens_by_collection', to: val+'#list_user_tokens_by_collection'
+        post 'user_token_detail', to: val+'#user_token_detail'
+        post 'list_users_by_token', to: val+'#list_users_by_token'
+      end
+      
+      post 'ver', to: val+'#version'
+      post 'log', to: val+'#log'
+      
+      scope :user do
+        post 'id', to: val+'#id'
+        post 'regist', to: val+'#regist'
+        post 'delete', to: val+'#delete'
+      end
+    end
+  }
 end
