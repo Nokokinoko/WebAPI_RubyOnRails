7afd78ac7111089fef3d8242d7ef02172769eb48
Syota Tsuda <s-tsuda@magicant.co.jp>
2022-11-15 03:01:46 +0900

    update before action, check request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  before_action :check_request
+  
+  private
+  def check_request
+    if request.content_type != "application/json"
+      render status: :method_not_allowed
+    end
+    
+    while false
+      if ENV.has_key?('docker')
+        break
+      end
+      
+      my_header = request.headers['X-MyHeader']
+      if my_header == 'MyHeaderCode'
+        break
+      end
+      
+      allow = [
+        '0.0.0.0',
+      ]
+      if allow.include?(request.remote_ip)
+        break
+      end
+      
+      render status: :method_not_allowed
+    end
+  end
 end
